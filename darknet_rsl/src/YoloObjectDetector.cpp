6ccdb6f2217e1ed696ba3ea0bdd0f10d9592696a
Marko Bjelonic <marko-bjelonic@t-online.de>
2016-12-22 09:24:27 +0100

    Deleted comments.
    Former-commit-id: 2eccb0fc1d6d7c71b6e67b655716909d3df257e9

--- darknet_rsl/src/YoloObjectDetector.cpp
+++ darknet_rsl/src/YoloObjectDetector.cpp
@@ -278,23 +278,10 @@ void YoloObjectDetector::cameraCallback(const sensor_msgs::ImageConstPtr& msg)
 
   if (cam_image)
   {
-    //cv::Point2f src_center(cam_image->image.cols/2.0F, cam_image->image.rows/2.0F);
-    //cv::Mat rot_mat = getRotationMatrix2D(src_center, 90, 1.0);
-    //cv::Mat dst;
-    //cv::warpAffine(cam_image->image, dst, rot_mat, cam_image->image.size());
     camImageCopy_ = cam_image->image.clone();
     frameWidth_ = cam_image->image.size().width;
     frameHeight_ = cam_image->image.size().height;
     runYolo(cam_image->image);
-
-    //int frameWidth = cam_image->image.size().width;
-    //int frameHeight = cam_image->image.size().height;
-    //cv::Rect myRoi((int)(frameWidth*0.14), (int)(frameHeight*0.01), (int)(frameWidth*0.75), (int)(frameHeight*0.99));
-    //cv::Mat cropedImage = cam_image->image(myRoi);
-    //frameWidth_ = cropedImage.size().width;
-    //frameHeight_ = cropedImage.size().height;
-    //camImageCopy_ = cropedImage.clone();
-    //runYolo(cropedImage);
   }
   return;
 }
