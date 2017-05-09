08bf4b033546a0b5cf12e8b2469b78d24f2d6db2
Marko Bjelonic <marko-bjelonic@t-online.de>
2017-05-09 17:21:15 +0200

    Free all images after every cycle.

--- darknet_ros/src/object_detector_demo.cpp
+++ darknet_ros/src/object_detector_demo.cpp
@@ -275,9 +275,10 @@ extern "C" darknet_ros::RosBox_ *demo_yolo()
     pthread_join(detect_thread, 0);
 
     free_image(disp);
-    disp  = det;
-    det   = in;
-    det_s = in_s;
+    free_image(in);
+    free_image(in_s);
+    free_image(det);
+
 
     tval_after = get_wall_time();
     float curr = 1./(tval_after - tval_before);
