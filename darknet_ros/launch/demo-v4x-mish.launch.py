6e04200d347a695ab17f9d2b19c967c63d09f7b0
Ar-Ray-code <ray255Ar@gmail.com>
2021-08-12 16:11:50 +0900

    launch concisely

--- darknet_ros/launch/demo-v4x-mish.launch.py
+++ darknet_ros/launch/demo-v4x-mish.launch.py
@@ -10,63 +10,24 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
   darknet_ros_share_dir = get_package_share_directory('darknet_ros')
+  network_param_file = darknet_ros_share_dir + '/config/yolov4-mish.yaml'
 
-  image = LaunchConfiguration('image', default = 'image_raw')
-  yolo_weights_path = LaunchConfiguration('yolo_weights_path', default = darknet_ros_share_dir + '/yolo_network_config/weights')
-  yolo_config_path = LaunchConfiguration('yolo_config_path', default = darknet_ros_share_dir + '/yolo_network_config/cfg')
-  ros_param_file = LaunchConfiguration('ros_param_file', default = darknet_ros_share_dir + 'config/ros.yaml')
-  network_param_file = LaunchConfiguration('network_param_file', default = darknet_ros_share_dir + 'config/yolov4x-mish.yaml')
-
-  declare_image_cmd = DeclareLaunchArgument(
-    'image',
-    default_value = '/image_raw',
-    description = 'Image topic')
-  declare_yolo_weights_path_cmd = DeclareLaunchArgument(
-    'yolo_weights_path',
-    default_value = darknet_ros_share_dir + '/yolo_network_config/weights',
-    description = 'Path to yolo weights') 
-  declare_yolo_config_path_cmd = DeclareLaunchArgument(
-    'yolo_config_path',
-    default_value = darknet_ros_share_dir + '/yolo_network_config/cfg',
-    description = 'Path to yolo config') 
-  declare_ros_param_file_cmd = DeclareLaunchArgument(
-    'ros_param_file',
-    default_value = darknet_ros_share_dir + '/config/ros.yaml',
-    description = 'Path to file with ROS related config')  
-  declare_network_param_file_cmd = DeclareLaunchArgument(
-    'network_param_file',
-    default_value = darknet_ros_share_dir + '/config/yolov4x-mish.yaml',
-    description = 'Path to file with network param file')  
-
-  darknet_ros_cmd = Node(
-    package='darknet_ros',
-    node_executable='darknet_ros',
-    node_name='darknet_ros',
-    output='screen',
-    parameters=[ros_param_file, network_param_file,
-      {
-        "config_path": yolo_config_path, 
-        "weights_path": yolo_weights_path,
-      },
-    ])
+  darknet_ros_launch = IncludeLaunchDescription(
+    PythonLaunchDescriptionSource([darknet_ros_share_dir + '/launch/darknet_ros.launch.py']),
+      launch_arguments={
+        'network_param_file': network_param_file,
+      }.items()
+  )
 
   camera = Node(
     package="v4l2_camera",
     node_executable="v4l2_camera_node",
-    node_namespace="camera/rgb",
+    # node_namespace="camera/rgb",
     parameters=[
       {'video_device'     : "/dev/video0"},
     ])
-
-  ld = LaunchDescription()
-
-  ld.add_action(declare_image_cmd)
-  ld.add_action(declare_yolo_weights_path_cmd)
-  ld.add_action(declare_yolo_config_path_cmd)
-  ld.add_action(declare_ros_param_file_cmd)
-  ld.add_action(declare_network_param_file_cmd)
   
-  ld.add_action(darknet_ros_cmd)
-  ld.add_action(camera)
-
-  return ld
+  return LaunchDescription([
+    darknet_ros_launch,
+    camera,
+  ])
