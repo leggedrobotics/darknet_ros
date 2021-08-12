fd6ff4a33579e7828e6b8b19a5b3728a2a6ae2da
Ar-Ray-code <ray255Ar@gmail.com>
2021-08-12 16:19:27 +0900

    fix launch

--- darknet_ros/launch/demo-v4-csp.launch.py
+++ darknet_ros/launch/demo-v4-csp.launch.py
@@ -1,11 +1,7 @@
-import os
-
 from ament_index_python.packages import get_package_share_directory
-
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable
+from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 def generate_launch_description():
@@ -14,15 +10,12 @@ def generate_launch_description():
 
   darknet_ros_launch = IncludeLaunchDescription(
     PythonLaunchDescriptionSource([darknet_ros_share_dir + '/launch/darknet_ros.launch.py']),
-      launch_arguments={
-        'network_param_file': network_param_file,
-      }.items()
+      launch_arguments={'network_param_file': network_param_file}.items()
   )
 
   camera = Node(
     package="v4l2_camera",
     node_executable="v4l2_camera_node",
-    # node_namespace="camera/rgb",
     parameters=[
       {'video_device'     : "/dev/video0"},
     ])
